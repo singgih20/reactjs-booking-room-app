527ef11ebb741885dff92930b9a979e98acaba05
Singgih <gokonggokong71@gmail.com>
2020-05-06 23:57:41 +0700

    add component footer

---
+++ src/parts/Footer.js
@@ -0,0 +1,87 @@
+import React from "react";
+import Button from "elements/Button";
+import IconText from "parts/IconText";
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col-auto" style={{ width: 350 }}>
+            <IconText />
+            <p className="brand-tag-line">
+              We kaboom your beauty holiday instantly and memorable.
+            </p>
+          </div>
+          <div className="col-auto mr-5">
+            <h6 className="mt-2">For Beginners</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button type="link" href="/register">
+                  New Account
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/properties">
+                  Start Booking a room
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/use-payment">
+                  Use Payments
+                </Button>
+              </li>
+            </ul>
+          </div>
+          <div className="col-auto mr-5">
+            <h6 className="mt-2">Explore Us</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button type="link" href="/career">
+                  Our Careers
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/privacy">
+                  Privacy
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/terms">
+                  Terms & Conditions
+                </Button>
+              </li>
+            </ul>
+          </div>
+          <div className="col-3 mr-5">
+            <h6 className="mt-2">Connect Us</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button
+                  type="link"
+                  isExternal
+                  href="mailto:support@staycation.id"
+                >
+                  support@staycation.id
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button isExternal type="link" href="tel:+6281317090804">
+                  081 - 1709 - 0804
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <span>Staycation, Kemang, Jakarta</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col text-center copyrights">
+            Copyright 2019 All Right Reserved
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
