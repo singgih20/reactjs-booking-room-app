86c5c21089d74cbd0bac11e0f1354ba3f659ff7d
Singgih <gokonggokong71@gmail.com>
2020-05-09 22:08:56 +0700

    component inputdate, inputnumber, breadcrumb

---
+++ src/pages/Example.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+import InputDate from "elements/InputForm/InputDate";
+
+export default class Example extends Component {
+  state = {
+    value: {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <InputDate
+          max={30}
+          onChange={this.handleChange}
+          name="value"
+          value={this.state.value}
+        />
+      </div>
+    );
+  }
+}
